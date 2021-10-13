99ed19e2329bbef87e731e84633b77ef20619694
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2021-10-13 11:31:17 +0100

    DB, FE, BE setup

---
+++ ultimateapi/database/migrations/2021_10_13_102606_create_user_infos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->unsignedBigInteger('loginId');
+
+            $table->foreign('loginid')
+                ->references(('id'))
+                ->on('logins')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+}
